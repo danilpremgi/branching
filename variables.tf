be32c30491823598e164aa476b7cb5ab26678ceb
Danil Premgi <35040511+danilpremgi@users.noreply.github.com>
2026-01-31 22:18:09 +0000

    Set B2ms VM sizes and Bastion Developer SKU

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group."
+  default     = "rg-uksouth-windows"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region to deploy resources into."
+  default     = "UKSouth"
+}
+
+variable "subscription_id" {
+  type        = string
+  description = "Azure subscription ID for the deployment."
+}
+
+variable "vm_size" {
+  type        = string
+  description = "Azure VM size."
+  default     = "Standard_B2ms"
+}
+
+variable "jumpbox_vm_size" {
+  type        = string
+  description = "Azure VM size for the jumpbox."
+  default     = "Standard_B2ms"
+}
+
+variable "admin_username" {
+  type        = string
+  description = "Admin username for the Windows VMs."
+  default     = "adm_azure"
+}
