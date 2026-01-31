b739913795f52f634a0c733178deda3d2c446d83
Danil Premgi <35040511+danilpremgi@users.noreply.github.com>
2026-01-31 14:25:35 +0000

    Add hub NAT gateway and jumpbox

---
+++ outputs.tf
@@ -0,0 +1,43 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.main.name
+}
+
+output "virtual_network_name" {
+  value = azurerm_virtual_network.main.name
+}
+
+output "hub_virtual_network_name" {
+  value = azurerm_virtual_network.hub.name
+}
+
+output "vm_names" {
+  value = azurerm_windows_virtual_machine.vm[*].name
+}
+
+output "vm_private_ips" {
+  value = azurerm_network_interface.vm[*].private_ip_address
+}
+
+output "secondary_vm_names" {
+  value = azurerm_windows_virtual_machine.secondary_vm[*].name
+}
+
+output "secondary_vm_private_ips" {
+  value = azurerm_network_interface.secondary_vm[*].private_ip_address
+}
+
+output "tertiary_virtual_network_name" {
+  value = azurerm_virtual_network.tertiary.name
+}
+
+output "tertiary_vm_name" {
+  value = azurerm_windows_virtual_machine.tertiary_vm.name
+}
+
+output "tertiary_vm_private_ip" {
+  value = azurerm_network_interface.tertiary_vm.private_ip_address
+}
+
+output "jumpbox_public_ip" {
+  value = azurerm_public_ip.jumpbox.ip_address
+}
